03860445a7c3f7392b26b60493715953d64c39aa
wetteyve <yveswetter@gmx.ch>
2024-07-12 13:14:03 +0200

    remove custom route

--- src/api/registration/controllers/registration.js
+++ src/api/registration/controllers/registration.js
@@ -6,11 +6,4 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::registration.registration", {
-  count(ctx) {
-    var { query } = ctx.request;
-    return strapi
-      .query("api::registration.registration")
-      .count({ where: query });
-  },
-});
+module.exports = createCoreController("api::registration.registration");
--- src/api/registration/routes/count-registrations.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  routes: [
-    {
-      method: "GET",
-      path: "/registrations/count",
-      handler: "registration.count",
-    },
-  ],
-};
